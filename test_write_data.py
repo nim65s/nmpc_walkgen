6987659b5d2dea96391b6667ae9b51587ccdc0bf
imaroger <imaroger@laas.fr>
2021-01-26 16:12:59 +0100

    add test to fill a data file

---
+++ test_write_data.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+f = open("demofile.dat", "w")
+f.write("1 2 3 4 5 \n")
+f.close()
+
+f = open("demofile.dat", "a")
+f.write("1 2 3 6 5 \n")
+f.close()
+
+human_traj = np.loadtxt("demofile.dat")
+print(human_traj)
